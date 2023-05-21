0decc3906492628f487657e7d8443eb3ac7cb448
Halezis <Halezis007@gmail.com>
2023-05-21 08:39:48 -0700

    commit4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != 'q' && x != 'e')
+		{
+			putchar(x);
+		}
+	}
+		putchar('\n');
+		return (0);
+}

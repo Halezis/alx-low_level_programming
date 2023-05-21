55e3c6de10d056ee564448c65c460dc9bb1e7e3b
Halezis <Halezis007@gmail.com>
2023-05-21 13:23:05 -0700

    commit6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  *main - Print a text using printf
+  *
+  *Return: Always 0
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a ");
+		printf("piece of art,\n");
+		return (0);
+}

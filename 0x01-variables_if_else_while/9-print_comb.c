e36de0a8d324765daf3fd64d781b20404f694b18
Halezis <Halezis007@gmail.com>
2023-05-21 10:38:51 -0700

    commit9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  *main - print single digit numbers, followed by a space
+  *
+  *Return: Always 0
+  */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 9; x++)
+	{
+		putchar('0' + x);
+		if (x < 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}

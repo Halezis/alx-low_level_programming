f5125e75b76c598e1c57447fe3d045a6eb5bc457
Halezis <Halezis007@gmail.com>
2023-05-21 22:18:35 -0700

    commit11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+  *main - Print combination sof 3 digits
+  *
+  *Return: Always 0
+  */
+
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x <= 7; x++)
+	{
+		for (y = x + 1; y <= 8; y++)
+		{
+			for (z = x + 2; z <= 9; z++)
+			{
+				putchar('0' + x);
+				putchar('0' + y);
+				putchar('0' + z);
+
+				if (x != 7 || y != 8 || z != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}

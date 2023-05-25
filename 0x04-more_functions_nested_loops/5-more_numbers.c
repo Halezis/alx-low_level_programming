cc4202cf7035ff62f09321c2af525d0d6d021ade
Halezis <Halezis007@gmail.com>
2023-05-25 10:52:45 -0700

    commit5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *more_number - Prints numbers 0 to 14 ten times
+ *
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 15; y++)
+		{
+			if (y > 9)
+			{
+				_putchar('0' + (y / 10));
+			}
+			_putchar('0' + (y % 10));
+		}
+		_putchar('\n');
+	}
+}

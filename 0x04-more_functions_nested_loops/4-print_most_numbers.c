c26dcb90da9f449aa298f787935d4dd15d5d9f1e
Halezis <Halezis007@gmail.com>
2023-05-25 10:38:09 -0700

    commit4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - check the code
+ *
+ *Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		if (x != 2 && x != 4)
+		{
+			_putchar('0' + x);
+		}
+	}
+	_putchar('\n');
+}

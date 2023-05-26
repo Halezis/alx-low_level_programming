22aa347d1b5ca911714dcdd741fe98e110606025
Halezis <Halezis007@gmail.com>
2023-05-26 11:21:52 -0700

    commit7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,11 +12,11 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (x = 0; x <= n; x++)
+		for (x = 1; x <= n; x++)
 		{
 			int y;
 
-			for (y = 0; y < x; y++)
+			for (y = 1; y < x; y++)
 			{
 				_putchar(' ');
 			}

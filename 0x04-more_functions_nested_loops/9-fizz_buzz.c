3ec0a4c7a2a87319c4febc821b57df37cbeeb3b5
Halezis <Halezis007@gmail.com>
2023-05-26 20:45:34 -0700

    commit9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,17 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (x < 10 && x % 3 == 0)
+		if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (x % 3 == 0)
 		{
 			printf("Fizz ");
-		}
-		else if (x < 10 && x % 3 != 0)
-		{
-			printf("%d ", x % 10);
 		}
 		else if (x % 5 == 0)
 		{
 			printf("Buzz ");
-		}
-		else if (x % 3 == 0 && x % 5 == 0)
-		{
-			printf("FizzBuzz ");
 		}
 		else
 		{

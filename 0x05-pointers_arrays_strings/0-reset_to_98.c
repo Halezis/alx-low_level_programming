27730a8b1fd5c1ed83fa391a958d340d6267d360
Halezis <Halezis007@gmail.com>
2023-05-29 18:50:53 -0700

    commit0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Updates value of a pointer to 98
+ *@n: Accepts values
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

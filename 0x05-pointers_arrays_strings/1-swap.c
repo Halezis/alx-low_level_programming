abe5ebcaf2b1b4c94d5fc66116f5a5d0f7d403c9
Halezis <Halezis007@gmail.com>
2023-05-29 19:24:35 -0700

    commit1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of two integers
+ *@a: Accepts value
+ *@b:Accepts value
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = *b;
+	*a = *b;
+	*b = x;
+}

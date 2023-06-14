0e83ed6cdf498644ed683c1616ba9707d9ac266c
Halezis <halezis007@gmail.com>
2023-06-14 05:46:34 +0100

    a function that frees a 2 dimensional grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,3 +8,20 @@
  *@height: y
  *Return: a pointer to the new string. Null, if str = NULL
 */
+
+void free_grid(int **grid, int height)
+{
+	int x;
+
+	if (grid == NULL || height <= 0)
+	{
+		return;
+	}
+
+	for (x = 0; x < height; x++)
+	{
+		free(grid[x]);
+	}
+
+	free(grid);
+}

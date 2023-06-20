1060258feceae69ca79bd9f2c708e6bbe090e4ad
Halezis <halezis007@gmail.com>
2023-06-20 02:52:43 +0100

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * struct dog - I dont know wtf a dog has a struct
+ * @name: It has a name too?
+ * @age: An age too, LMAO?
+ * @owner: Yeah, curious to know who owns him
+*/
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
